275ef886fb535ef01aea68cc5553677ddb76b7c6
Bekir - CDI <b.burakcebecii@gmail.com>
2022-11-29 16:30:10 +0100

    input, echo and charecter count is ready..

---
+++ src/routes/CharacterCounter.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { atom, selector, useRecoilState, useRecoilValue } from "recoil";
+
+const textState = atom({
+  key: "textState", // unique ID (with respect to other atoms/selectors)
+  default: "", // default value (aka initial value)
+});
+
+const charCountState = selector({
+  key: "charCountState", // unique ID (with respect to other atoms/selectors)
+  get: ({ get }) => {
+    const text = get(textState);
+
+    return text.length;
+  },
+});
+
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+
+  return <>Character Count: {count}</>;
+}
+
+export const CharacterCounter = function () {
+  return (
+    <div>
+      <TextInput />
+      <CharacterCount />
+    </div>
+  );
+};
+
+export const TextInput = function () {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  );
+};
